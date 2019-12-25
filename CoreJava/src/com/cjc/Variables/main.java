b58b45797936df9e580d61d9d131e62a1000e49c
ashup1792 <ash99patil@gmail.com>
2019-12-25 14:59:56 +0530

    first commit upload all files

---
+++ CoreJava/src/com/cjc/Variables/main.java
@@ -0,0 +1,16 @@
+package com.cjc.Variables;
+
+public class main {
+	 public static void main(String[] args) {
+		System.out.println("String []");
+		
+		
+	}
+	 public static void main(int [] args) {
+		 System.out.println("int []");
+		
+	}
+
+}
+
+
---
+++ CoreJava/src/com/cjc/Variables/variable.java
@@ -0,0 +1,41 @@
+package com.cjc.Variables;
+/*class variable1
+{
+	static int y;	
+	int x;
+
+
+}*/
+
+public class variable extends main {
+	static int x;;
+	
+	int y;
+	public static void main(String[] args) {
+		
+		
+		
+		//System.out.println(x);;
+		variable v=new variable();
+		System.out.println(x);
+		System.out.println(v.y);
+	
+	
+	/*variable1 v1=new variable1();
+	System.out.println(v1.x);
+	System.out.println(v1.y);
+	*/
+		
+		
+	variable v2=new variable();
+	System.out.println(x);
+	System.out.println(v.y);
+	
+	for(int z=0;z<3;z++)
+	{
+		System.out.println(z);
+	}
+	
+	}
+
+}
